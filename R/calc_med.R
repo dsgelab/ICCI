f6f988e0fd905cfdd43b5a6191a8429930d5fc72
Kira Detrois <kira.detrois@helsinki.fi>
2022-12-28 14:42:47 +0200

    Changed MI to MED

--- R/calc_med.R
+++ R/calc_med.R
@@ -7,18 +7,18 @@
 #' @export
 #' 
 #' @author Kira E. Detrois
-calc_mi <- function(atc_data,
+calc_med <- function(atc_data,
                     exp_start,
                     exp_end) {
-    mi_data <- get_exposure_data(atc_data, 
+    med_data <- get_exposure_data(atc_data, 
                                  exp_start, 
                                  exp_end) %>%
                         dplyr::select(ID, ATC, WEIGHT) %>% 
                         dplyr::distinct()
-    mi_data$WEIGHT[is.na(mi_data$WEIGHT)] <- 0
-    mi_data <- dplyr::group_by(mi_data, ID) %>% 
+    med_data$WEIGHT[is.na(med_data$WEIGHT)] <- 0
+    med_data <- dplyr::group_by(med_data, ID) %>% 
                 dplyr::summarise(SCORE=sum(WEIGHT))
-    mi_data <- add_back_missing_indvs(atc_data, mi_data)
-    mi_data <- dplyr::rename(mi_data, MI=SCORE)
-    return(mi_data)
+    med_data <- add_back_missing_indvs(atc_data, med_data)
+    med_data <- dplyr::rename(med_data, MED=SCORE)
+    return(med_data)
 }
--- tests/testthat/test-calc_med.R
+++ tests/testthat/test-calc_med.R
@@ -1,19 +1,19 @@
 test_that("multiplication works", {
   atc_data <- create_test_atc_data(c("KT0001", "KT0008", "KT0007", "KT0005", "KT0003"), n_samples=10)
   
-  mi_data <- calc_mi(atc_data, 
+  med_data <- calc_med(atc_data, 
                    exp_start=0, 
                    exp_end=100)
   expect_res <- tibble::tibble(ID=c("KT0005", "KT0001", "KT0008", "KT0003"),
-                               MI=c(0,1,-2,0))
-  expect_equal(mi_data, expect_res)
+                               MED=c(0,1,-2,0))
+  expect_equal(med_data, expect_res)
 
 
-  mi_data <- calc_mi(atc_data, 
+  med_data <- calc_med(atc_data, 
                      exp_start=50, 
                      exp_end=70)
   expect_res <- tibble::tibble(ID=c("KT0005", "KT0001", "KT0008", "KT0003"),
-                               MI=c(0,1,0,0))
+                               MED=c(0,1,0,0))
 
-  expect_equal(mi_data, expect_res)
+  expect_equal(med_data, expect_res)
 })
